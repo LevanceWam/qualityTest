4b4841bedf593af45a176fe71524a2ad7227fedc
seanedw1 <sedwards@fullsail.edu>
2016-10-20 03:55:11 -0400

    updated event stream

--- src/util.js
+++ src/util.js
@@ -1,16 +1,21 @@
-exports.debug = (title, obj, data) => {
+exports.debug = (title, obj, status) => {
 
   const ts = new Date();
   const seperator = '\n____________________________________\n';
   const require('terminal-colors');
 
-  const outputTERM = seperator.magenta + ' ' + JSON.stringify(obj) + ' ' + data + '\n'
-   + seperator.magenta + ' ' + ts;
-
-  const outputFILE = seperator + ' ' + JSON.stringify(obj) + ' ' +
-  data + '\n' + seperator + ' ' + ts;
   if (process.env.DEBUG) {
-
-      console.log(outputTERM);
+    let outputTERM;
+    if (status === 'error') {
+       outputTERM = seperator.red + ' ' + ts + '\n' + JSON.stringify(obj) + ' ' + seperator.red;
+        // if console warn
+        } else if (status === 'warn') {
+        outputTERM = seperator.yellow + ' ' + ts + '\n' + JSON.stringify(obj) + ' ' + seperator.magenta;
+        // if console log or undefined
+        } else {
+         outputTERM = seperator.green + ' ' + ts + '\n' + JSON.stringify(obj) + ' ' + seperator.green;
+        // closes if statement
+        }
   }
 };
+console.log(outputTERM);
